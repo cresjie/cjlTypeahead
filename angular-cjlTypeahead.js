a090d3d5aab754fe4342035aeae5700631fba021
Cres Jie <cresjie@gmail.com>
2015-08-04 23:51:55 +0800

    Create angular-cjlTypeahead.js

---
+++ angular-cjlTypeahead.js
@@ -0,0 +1,27 @@
+(function(angular, window,directiveName){
+	angular.module('cjl.typeahead',[]).directive(directiveName,function(){
+		return {
+			restrict: 'AC',
+			
+			link:function(scope, element,attrs){
+				
+					element.typeahead(scope.$eval(attrs.typeaheadOptions), scope.$eval(attrs.typeaheadDatasets) );
+
+					element.$on = function(event, callback){
+						element.on(event,function(event, obj, obj2){
+							callback(event, obj, obj2);
+							scope.$apply();
+						});
+					};
+
+					if( !scope.typeahead )
+						scope.typeahead = {};
+
+					scope.typeahead[attrs[directiveName]] = element;
+					
+
+				}
+			
+		};
+	});
+})(angular,window,'cjlTypeahead');
